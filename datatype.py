22d5eda0cec241be9f0f6a75cb05602fb6f30ea6
unknown <chockymoni98@gmail.com>
2025-10-31 17:35:33 +0530

    uploading practiced task

---
+++ datatype.py
@@ -0,0 +1,26 @@
+name="Monisha"
+age=26
+height=165.55
+weight=75
+city="Madurai"
+is_student=True
+marks=82
+pi=3.14
+school_name="Keren Matriculation higher secondary school"
+my_number=10
+my_text="Hello"
+print("My name is:",name)
+print("My age is:",age)
+print("My height is:",height)
+print("My weight is:",weight)
+print("My city name is :",city)
+print("I am a ",is_student)
+print("My mark is:",marks)
+print("The pi value is:",pi)
+print("My School name is:",school_name)
+print("Name is:",name,",type",type(name))
+print("Age is:",age,",type",type(age))
+print("PI Value is:",pi, ",type",type(pi))
+print("Is a :",is_student, ",type",type(is_student))
+print(my_number,",type",type(my_number))
+print(my_text,",type",type(my_text))
---
+++ mark.py
@@ -0,0 +1,4 @@
+print("My 10th grade mark is 462")
+print("My 12th grade mark is 868")
+print("My bachelores degree percentage is 64%")
+print("My post graduation degree percentage is 82%")
